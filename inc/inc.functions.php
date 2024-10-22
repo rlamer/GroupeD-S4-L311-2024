d2b574dd8f959b850ab576262a5a36e93abf0751
LAMER Rémi <remi.lamer@etu.unilim.fr>
2024-10-22 17:30:10 +0200

    Première correction fichier inc.functions

--- inc/inc.functions.php
+++ inc/inc.functions.php
@@ -20,7 +20,7 @@
 
     function setDisconnectUser(){
          unset($_SESSION['User']);
-         sessions_destroy();
+         session_destroy();    // Sessions => session
     }
 
     function isConnected(){
@@ -36,24 +36,24 @@
         $fichier = TL_ROOT.'/pages/'.(is_null($page) ? 'index.php' : $page.'.php');
 
         if(!file_exists($fichier)){
-            inclde TL_ROOT.'/pages/index.php';
+            include TL_ROOT.'/pages/index.php';
         }else{
             include $fichier;
         }
     }
 
     function getArticlesFromJson(){
-        if(file_exist(DB_ARTICLE)) {
-            $contenu_json = file_get_contents(DB_ARTICLE);
+        if(file_exists(DB_ARTICLES)) {   // Correction "file_exist" et "DB_ARTICLES" au pluriel
+            $contenu_json = file_get_contents(DB_ARTICLES);
             return json_decode($contenu_json, true);
         }
 
         return null;
     }
 
-    function getArticleById($id_article == null){
-       if(file_exists(DB_ARTICLE)) {
-            $contenu_json = file_get_contents(DB_ARTICLE);
+    function getArticleById($id_article = null){  // Un "=" en trop + Mise au pluriel de "DB_ARTICLES" comme précédemment
+       if(file_exists(DB_ARTICLES)) {
+            $contenu_json = file_get_contents(DB_ARTICLES);
             $_articles    = json_decode($contenu_json, true);
 
             foreach($_articles as $article){
